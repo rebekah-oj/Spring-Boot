930f29a3ec0c759fdf548135ed3ad27f984bacaf
tap <olojadeborah37@gmail.com>
2020-11-26 21:49:09 +0100

    error fix

--- src/main/java/com/company/Entity/Student.java
+++ src/main/java/com/company/Entity/Student.java
@@ -1,4 +1,4 @@
-package Entity;
+package com.company.Entity;
 
 public class  Student {
 
--- src/main/java/com/company/Main.java
+++ src/main/java/com/company/Main.java
@@ -1,13 +1,15 @@
-package Main;
+package com.company;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-//lets compiler know it's a spring boot application
 @SpringBootApplication
 public class Main {
 
     public static void main(String[] args) {
+
         SpringApplication.run(Main.class, args);
+
     }
+
 }
